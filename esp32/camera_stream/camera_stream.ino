719d3827aff27cf10be048efadd5486c951e9f0b
Nono0325 <nono0325@github.com>
2026-07-16 10:37:30 +0800

    fix: revert fb_count to 1 and set xclk_freq_hz to 6MHz to prevent stack panic and DMA overflow

--- esp32/camera_stream/camera_stream.ino
+++ esp32/camera_stream/camera_stream.ino
@@ -190,11 +190,11 @@ void setup() {
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 10000000;
+  config.xclk_freq_hz = 6000000;
   config.pixel_format = PIXFORMAT_JPEG;
   config.frame_size = FRAMESIZE_QVGA; 
   config.jpeg_quality = 15; 
-  config.fb_count = 2;
+  config.fb_count = 1;
   config.fb_location = CAMERA_FB_IN_DRAM; 
   
   Serial.println("Initialzing ESP32-CAM Safety System (Optimized)...");
